6def8b606056a238bfb8103998efc378659227d4
Shadman Sohel <shadman.sohel04@gmail.com>
2024-08-17 15:02:19 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GamePage from './pages/GamePage';
 import GameStart from './pages/gameStart';
+import HomePage from './pages/homepage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<GamePage />} />
+        <Route index element={<HomePage />} />
         <Route path='/lobby' element={<GameStart />} />
+        <Route path='/lobby/game' element={<GamePage />}/>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/GameStart.jsx
+++ src/pages/GameStart.jsx
@@ -1,38 +1,60 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
-
+import GamePage from "./GamePage";
 
 export default function GameStart() {
-
     const [message, setMessage] = useState("");
     const [socketId, setSocketId] = useState("");
+    const [socket, setSocket] = useState(null);
+    const [lobby, setLobby] = useState(true)
+    const [game, setGame] = useState(false)
 
     useEffect(() => {
-        const socket = io.connect("http://192.168.2.220:3001");
+        const newSocket = io.connect("http://192.168.2.220:3001");
+        setSocket(newSocket);
 
-        socket.on("connect", () => {
-            setSocketId(socket.id);
-            localStorage.setItem("id", socket.id)
-            console.log(`Connected with socket ID: ${socket.id}`);
+        newSocket.on("connect", () => {
+            setSocketId(newSocket.id);
+            localStorage.setItem("id", newSocket.id);
+            console.log(`Connected with socket ID: ${newSocket.id}`);
         });
 
-
-        socket.on("message", (data) => {
+        newSocket.on("message", (data) => {
             setMessage(data);
         });
 
-        // Cleanup on component unmount
         return () => {
-            socket.off("message");
-            socket.off("connect");
+            newSocket.off("message");
+            newSocket.off("connect");
+            newSocket.disconnect(); // Properly close the socket connection
         };
     }, []);
 
+    useEffect(() => {
+        if (socket) {
+            socket.on("sendTogame", (data) => {
+                setLobby(false)
+                setGame(true)
+            });
+
+            return () => {
+                socket.off("sendTogame");
+            };
+        }
+    }, [socket]);
+
     return (
         <div>
-            <h1>Game Start</h1>
-            <p>Socket ID: {socketId}</p>
-            <p>Message from server: {message}</p>
+            {lobby && (
+                <div>
+                    <h1>Game Start</h1>
+                    <p>Socket ID: {socketId}</p>
+                    <p>Message from server: {message}</p>
+                </div>
+            )}
+            {game && <GamePage />}
+
         </div>
+        
     );
 }
